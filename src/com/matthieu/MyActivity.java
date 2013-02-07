133424686107144a2ec78a06ccb6f3301fe4017b
Matthieu Jeanson <matthieu@touchrev.com>
2013-02-07 13:05:27 -0800

    Initial commit

---
+++ src/com/matthieu/MyActivity.java
@@ -0,0 +1,16 @@
+package com.matthieu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.HorizontalScrollView;
+
+public class MyActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
